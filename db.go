60065d22b9fc114910fbe8db558b2148e497dd34
Marcel Voigt <mv@noch.so>
2016-05-01 03:24:08 +0200

    Add name column to address

--- db.go
+++ db.go
@@ -38,13 +38,13 @@ func prepareDatabase(db *sql.DB) {
 	}
 	mlog.Info("Setting up database schema")
 	_, err = db.Exec(`
-CREATE TABLE address
-(
-    id integer PRIMARY KEY NOT NULL,
-    address TEXT NOT NULL
+CREATE TABLE "address" (
+  "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+  "address" text NOT NULL,
+  "name" text NOT NULL
 );
-CREATE UNIQUE INDEX "uidx_address_address" ON "address" ("address");
 CREATE INDEX "idx_address_address" ON "address" ("address");
+CREATE UNIQUE INDEX "uidx_address_address" ON "address" ("address");
 CREATE TABLE "mail" (
   "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
   "sender_id" integer NOT NULL,
@@ -54,18 +54,18 @@ CREATE TABLE "mail" (
   "is_deleted" integer NOT NULL,
   "ts_deleted" integer NOT NULL
 );
-CREATE INDEX "idx_mail_ts_received" ON "mail" ("ts_received");
-CREATE INDEX "idx_mail_sender_id" ON "mail" ("sender_id");
 CREATE INDEX "idx_mail_is_deleted_ts_deleted" ON "mail" ("is_deleted", "ts_deleted");
+CREATE INDEX "idx_mail_sender_id" ON "mail" ("sender_id");
+CREATE INDEX "idx_mail_ts_received" ON "mail" ("ts_received");
 CREATE TABLE mail_recipient
 (
     id integer PRIMARY KEY NOT NULL,
     mail_id integer NOT NULL,
     recipient_id integer NOT NULL
 );
-CREATE INDEX "idx_mail_recipient_recipient_id" ON "mail_recipient" ("recipient_id");
-CREATE INDEX "idx_mail_recipient_mail_id" ON "mail_recipient" ("mail_id");
 CREATE UNIQUE INDEX "uidx_mail_recipient_mail_id_recipient_id" ON "mail_recipient" ("mail_id", "recipient_id");
+CREATE INDEX "idx_mail_recipient_mail_id" ON "mail_recipient" ("mail_id");
+CREATE INDEX "idx_mail_recipient_recipient_id" ON "mail_recipient" ("recipient_id");
 `)
 	if err != nil {
 		mlog.Fatalf("Unable to set up database schema: %s", err)
