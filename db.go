0d49f8d506f123cb8e16db0490d5d274a80c5922
Marcel Voigt <mv@noch.so>
2016-05-01 14:54:31 +0200

    Drop mail_recipient table

--- db.go
+++ db.go
@@ -62,29 +62,38 @@ CREATE TABLE "address" (
   "address" text NOT NULL,
   "name" text NOT NULL
 );
-CREATE INDEX "idx_address_address" ON "address" ("address");
 CREATE UNIQUE INDEX "uidx_address_address" ON "address" ("address");
+CREATE INDEX "idx_address_address" ON "address" ("address");
 CREATE TABLE "mail" (
   "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
   "sender_id" integer NOT NULL,
+  "recipient_id" integer NOT NULL,
   "content" text NOT NULL,
   "ts_received" integer NOT NULL,
   "subject" text NOT NULL,
   "is_deleted" integer NOT NULL,
-  "ts_deleted" integer NOT NULL
+  "ts_deleted" integer NULL
 );
-CREATE INDEX "idx_mail_is_deleted_ts_deleted" ON "mail" ("is_deleted", "ts_deleted");
-CREATE INDEX "idx_mail_sender_id" ON "mail" ("sender_id");
 CREATE INDEX "idx_mail_ts_received" ON "mail" ("ts_received");
-CREATE TABLE mail_recipient
-(
-    id integer PRIMARY KEY NOT NULL,
-    mail_id integer NOT NULL,
-    recipient_id integer NOT NULL
-);
-CREATE UNIQUE INDEX "uidx_mail_recipient_mail_id_recipient_id" ON "mail_recipient" ("mail_id", "recipient_id");
-CREATE INDEX "idx_mail_recipient_mail_id" ON "mail_recipient" ("mail_id");
-CREATE INDEX "idx_mail_recipient_recipient_id" ON "mail_recipient" ("recipient_id");
+CREATE INDEX "idx_mail_sender_id" ON "mail" ("sender_id");
+CREATE INDEX "idx_mail_is_deleted_ts_deleted" ON "mail" ("is_deleted", "ts_deleted");
+CREATE TABLE "v_mail" ("id" integer, "subject" text, "content" text, "received" , "deleted" , "sender" text, "recipient" text, "is_deleted" integer);
+DROP TABLE IF EXISTS "v_mail";
+CREATE VIEW "v_mail" AS
+SELECT
+  mail.id,
+  mail.subject,
+  mail.content,
+  datetime(mail.ts_received, 'unixepoch', 'localtime')         AS received,
+  CASE WHEN mail.ts_deleted IS NULL
+    THEN ''
+  ELSE datetime(mail.ts_deleted, 'unixepoch', 'localtime') END AS deleted,
+  s_addr.address                                               AS sender,
+  r_addr.address                                               AS recipient,
+  mail.is_deleted
+FROM mail
+  INNER JOIN address s_addr ON mail.sender_id = s_addr.id
+  INNER JOIN address r_addr ON mail.recipient_id = r_addr.id;
 `)
 	if err != nil {
 		mlog.Fatalf("Unable to set up database schema: %s", err)
