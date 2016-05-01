33db3ccae5f22ef69ae17f4d95d7139bf701c1f7
Marcel Voigt <mv@noch.so>
2016-05-01 03:21:30 +0200

    Use 64 bit ints

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-xo sqlite3://mail.sqlite -o models
+xo sqlite3://data/mail.sqlite -o models --int32-type int64 --uint32-type uint64
 
 # With tag: 1.0.0[-commits since tag][-dirty]
 # Without existing tags: 4a4154a[-dirty]
@@ -6,4 +6,3 @@ version=$(git describe --tags --always --dirty)
 # 2000-12-13 13:00:00
 buildDate=$(date --utc "+%F %T")
 go build -v -ldflags "-X 'main.Version=$version' -X 'main.BuildDate=$buildDate'"
-#go build -v
