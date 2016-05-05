ebf5c8c826b350eb847c1a4fe6c50b4e9e4502c0
Marcel Voigt <mv@noch.so>
2016-05-05 22:17:49 +0200

    Add custom xo query for flag

--- build.sh
+++ build.sh
@@ -3,6 +3,12 @@ rm data/mail.sqlite models/*.xo.go
 go-bindata model.sql LICENSE
 cat model.sql | sqlite3 -batch data/mail.sqlite
 xo sqlite3://data/mail.sqlite -o models --int32-type int64 --uint32-type uint64
+xo sqlite3://data/mail.sqlite -o models -a -N -M -B -T Flag -F GetFlags << ENDSQL
+SELECT
+  f.id,
+  f.name
+FROM flag f
+ENDSQL
 
 # With tag: 1.0.0[-commits since tag][-dirty]
 # Without existing tags: 4a4154a[-dirty]
