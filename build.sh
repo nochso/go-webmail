86df58bee114f3d1d0ed316ddddffab92e84a70c
Marcel Voigt <mv@noch.so>
2016-05-05 12:23:57 +0200

    Run sqlite in batch mode

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cat model.sql | sqlite3 -interactive data/mail.sqlite
+cat model.sql | sqlite3 -batch data/mail.sqlite
 xo sqlite3://data/mail.sqlite -o models --int32-type int64 --uint32-type uint64
 
 # With tag: 1.0.0[-commits since tag][-dirty]
